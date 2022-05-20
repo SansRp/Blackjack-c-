9a07843e699706b47a62e4d9e392390725a1e242
santiagorossetto <santiagoroseto@gmail.com>
2022-05-20 18:53:16 -0300

    blackjack c#

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Botones
+    public Button dealBtn;
+    public Button hitBtn;
+    public Button standBtn;
+    public Button betBtn;
+
+    private int standClicks = 0;
+
+    // Acceso a los codigos del jugador y el crupier
+    public PlayerScript playerScript;
+    public PlayerScript dealerScript;
+
+    // hud
+    public Text scoreText;
+    public Text dealerScoreText;
+    public Text betsText;
+    public Text cashText;
+    public Text mainText;
+    public Text standBtnText;
+
+    // esconde la segunda carta del dealer
+    public GameObject hideCard;
+    // cuanto es la apuesta
+    int pot = 0;
+
+    void Start()
+    {
+        // Agrega Click listener a los botones
+        dealBtn.onClick.AddListener(() => DealClicked());
+        hitBtn.onClick.AddListener(() => HitClicked());
+        standBtn.onClick.AddListener(() => StandClicked());
+        betBtn.onClick.AddListener(() => BetClicked());
+    }
+
+    private void DealClicked()
+    {
+        // Resetea la ronda, esconde las cartas y prepara para la proxima ronda
+        playerScript.ResetHand();
+        dealerScript.ResetHand();
+        // Esconde el boton Jugar y los puntos del crupier
+        dealerScoreText.gameObject.SetActive(false);
+        mainText.gameObject.SetActive(false);
+        dealerScoreText.gameObject.SetActive(false);
+        GameObject.Find("Deck").GetComponent<DeckScript>().Shuffle();
+        playerScript.StartHand();
+        dealerScript.StartHand();
+        // Actualiza el puntaje total
+        scoreText.text = "Hand: " + playerScript.handValue.ToString();
+        dealerScoreText.text = "Hand: " + dealerScript.handValue.ToString();
+        hideCard.GetComponent<Renderer>().enabled = true;
+        // Aumenta la visibilidad de los botones
+        dealBtn.gameObject.SetActive(false);
+        hitBtn.gameObject.SetActive(true);
+        standBtn.gameObject.SetActive(true);
+        standBtnText.text = "Stand";
+        // tamaño estandar de la canasta
+        pot = 40;
+        betsText.text = "Bets: $" + pot.ToString();
+        playerScript.AdjustMoney(-20);
+        cashText.text = "$" + playerScript.GetMoney().ToString();
+
+    }
+
+    private void HitClicked()
+    {
+
+        if (playerScript.cardIndex <= 10)
+        {
+            playerScript.GetCard();
+            scoreText.text = "Hand: " + playerScript.handValue.ToString();
+            if (playerScript.handValue > 20) RoundOver();
+        }
+    }
+
+    private void StandClicked()
+    {
+        standClicks++;
+        if (standClicks > 1) RoundOver();
+        HitDealer();
+        standBtnText.text = "Call";
+    }
+
+    private void HitDealer()
+    {
+        while (dealerScript.handValue < 16 && dealerScript.cardIndex < 10)
+        {
+            dealerScript.GetCard();
+            dealerScoreText.text = "Hand: " + dealerScript.handValue.ToString();
+            if (dealerScript.handValue > 20) RoundOver();
+        }
+    }
+
+    // Determina el ganador y el perdedor (termina la ronda)
+    void RoundOver()
+    {
+       
+        bool playerBust = playerScript.handValue > 21;
+        bool dealerBust = dealerScript.handValue > 21;
+        bool player21 = playerScript.handValue == 21;
+        bool dealer21 = dealerScript.handValue == 21;
+        
+        if (standClicks < 2 && !playerBust && !dealerBust && !player21 && !dealer21) return;
+        bool roundOver = true;
+    
+        if (playerBust && dealerBust)
+        {
+            mainText.text = "Ambos Pierden";
+            playerScript.AdjustMoney(pot / 2);
+        }
+      
+        else if (playerBust || (!dealerBust && dealerScript.handValue > playerScript.handValue))
+        {
+            mainText.text = "Gana el Crupier!";
+        }
+       
+        else if (dealerBust || playerScript.handValue > dealerScript.handValue)
+        {
+            mainText.text = "Ganaste!";
+            playerScript.AdjustMoney(pot);
+        }
+      
+        else if (playerScript.handValue == dealerScript.handValue)
+        {
+            mainText.text = "Tenemos un Empate!";
+            playerScript.AdjustMoney(pot / 2);
+        }
+        else
+        {
+            roundOver = false;
+        }
+        
+        if (roundOver)
+        {
+            hitBtn.gameObject.SetActive(false);
+            standBtn.gameObject.SetActive(false);
+            dealBtn.gameObject.SetActive(true);
+            mainText.gameObject.SetActive(true);
+            dealerScoreText.gameObject.SetActive(true);
+            hideCard.GetComponent<Renderer>().enabled = false;
+            cashText.text = "$" + playerScript.GetMoney().ToString();
+            standClicks = 0;
+        }
+    }
+
+    // agregar dinero ala canastas si se toca la ficha de apuesta
+    void BetClicked()
+    {
+        Text newBet = betBtn.GetComponentInChildren(typeof(Text)) as Text;
+        int intBet = int.Parse(newBet.text.ToString().Remove(0, 1));
+        playerScript.AdjustMoney(-intBet);
+        cashText.text = "$" + playerScript.GetMoney().ToString();
+        pot += (intBet * 2);
+        betsText.text = "Bets: $" + pot.ToString();
+    }
+}
---
+++ Scripts/PlayerScript.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // --- Este codigo es para el jugador y el crupier
+
+    // Uso de otros scripts
+    public CardScript cardScript;   
+    public DeckScript deckScript;
+
+    // Valor total del jugador y el crupier
+    public int handValue = 0;
+
+    // Dinero de apuestas
+    private int money = 1000;
+
+    // Vector de la carta objeto en la mesa
+    public GameObject[] hand;
+    // Indice de la proxima carta a dar vuelta
+    public int cardIndex = 0;
+    // Identificador de los aces a covertir de 1 a 11
+    List<CardScript> aceList = new List<CardScript>();
+
+    public void StartHand()
+    {
+        GetCard();
+        GetCard();
+    }
+
+
+    public int GetCard()
+    {
+        // Consigue una carta , asignar el valor y imagen a la carta que levantaste
+        int cardValue = deckScript.DealCard(hand[cardIndex].GetComponent<CardScript>());
+        // Muesta la carta en pantalla
+        hand[cardIndex].GetComponent<Renderer>().enabled = true;
+        // Agrega el valor de la carta al valor total de la mano
+        handValue += cardValue;
+        // si el valor es 1 es un as
+        if(cardValue == 1)
+        {
+            aceList.Add(hand[cardIndex].GetComponent<CardScript>());
+        }
+        // Revisa si deberia usar un 11 en ves de un 1
+        AceCheck();
+        cardIndex++;
+        return handValue;
+    }
+
+    
+    public void AceCheck()
+    {
+     
+        foreach (CardScript ace in aceList)
+        {
+            if(handValue + 10 < 22 && ace.GetValueOfCard() == 1)
+            {
+             
+                ace.SetValue(11);
+                handValue += 10;
+            } else if (handValue > 21 && ace.GetValueOfCard() == 11)
+            {
+             
+                ace.SetValue(1);
+                handValue -= 10;
+            }
+        }
+    }
+
+    // agregar o quitar dinero de la cantidad total
+    public void AdjustMoney(int amount)
+    {
+        money += amount;
+    }
+
+    // muestra la cantidad de dinero del jugador
+    public int GetMoney()
+    {
+        return money;
+    }
+
+    // esconder todas las cartas y resetea las variables utilisadas
+    public void ResetHand()
+    {
+        for(int i = 0; i < hand.Length; i++)
+        {
+            hand[i].GetComponent<CardScript>().ResetCard();
+            hand[i].GetComponent<Renderer>().enabled = false;
+        }
+        cardIndex = 0;
+        handValue = 0;
+        aceList = new List<CardScript>();
+    }
+}
